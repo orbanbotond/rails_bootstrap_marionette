44bb903f28420edf5a1603961625468adccbfeb8
Orban Botond <orbanbotond@gmail.com>
2013-07-10 15:04:05 +0300

    Initial commit

---
+++ app/assets/javascripts/bootstrapbackbonemaionette.js.coffee
@@ -0,0 +1,9 @@
+window.Bootstrapbackbonemaionette =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Bootstrapbackbonemaionette.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bootstrapbackbonemaionette::Application.config.secret_key_base = '5ab65fd5cb2b474f3e83a2b8b498a58c07cffdd9aa571079fcf843d5f17b4b74cb3746ef79a9df670e8d96f40540fbac77a71af424131df742ea7955ec6f69e8'
